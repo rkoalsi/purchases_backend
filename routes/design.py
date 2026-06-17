e08177c5f7e3cb65a471265aa974f1cd0b3affb0
rkoalsi <57904969+rkoalsi@users.noreply.github.com>
2026-06-17 18:00:19 +0530

    fixing upsert

--- routes/design.py
+++ routes/design.py
@@ -1106,7 +1106,7 @@ def _process_pis_sheet(ws, sheet_name: str, db, dry_run: bool = False) -> dict:
                     db[DESIGN_CATALOGUE_COLLECTION].update_one(
                         {"bb_code": bb_code},
                         {
-                            "$setOnInsert": {"product_id": str(product["_id"])},
+                            "$setOnInsert": {"product_id": product["_id"]},
                             "$set": set_fields,
                         },
                         upsert=True,
